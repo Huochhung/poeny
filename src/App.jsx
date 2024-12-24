e9d67bf85aa09646720585bb14f9a81af69cc8fb
Huochhung <youhuochhung@gmail.com>
2024-12-24 13:46:40 +0700

    update all files

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,18 @@ import Login from "./pages/Login";
 import Orders from "./pages/Orders";
 import PlaceOrder from "./pages/PlaceOrder";
 import Product from "./pages/Product";
+import Footer from "./components/footer";
+import SearchBar from "./components/SearchBar";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <Navbar />
+      <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -26,6 +33,7 @@ const App = () => {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/product/:productId" element={<Product />} />
       </Routes>
+      <Footer/>
     </div>
   );
 };
